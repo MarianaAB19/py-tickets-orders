1c03e156a1125c573d1dcc24038302a7652771ce
Mariana <mariana.antoniak.babish@gmail.com>
2025-10-07 12:18:49 +0300

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -85,13 +85,20 @@ class Ticket(models.Model):
     seat = models.IntegerField()
 
     @staticmethod
-    def validate_place(value, num_attr, attr_name, hall_attr_name, error_to_raise):
+    def validate_place(
+            value,
+            num_attr,
+            attr_name,
+            hall_attr_name,
+            error_to_raise
+    ):
         if not (1 <= value <= num_attr):
             raise error_to_raise(
                 {
                     attr_name: f"{attr_name} "
                     f"number must be in available range: "
-                    f"(1, {num_attr}) for cinema hall attribute {hall_attr_name}."
+                    f"(1, {num_attr}) "
+                    f"for cinema hall attribute {hall_attr_name}."
                 }
             )
 
